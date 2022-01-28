1c688b969565e9ff0685732c9ff56bc0b3676a33
Puker Who <nawwesele@gmail.com>
2022-01-28 11:42:07 +0200

    edit

--- archive.php
+++ archive.php
@@ -13,6 +13,12 @@
 						<!-- END Post item -->
 						<?php endwhile; endif; wp_reset_postdata(); ?>
 					</div>	
+					<div class="mb-6">
+						<div class="flex justify-between items-center prose-a:inline-block prose-a:bg-indigo-500 prose-a:text-gray-200 prose-a:rounded prose-a:px-6 prose-a:py-3">
+							<?php posts_nav_link(); ?>	
+						</div>
+						
+					</div>
         </div>
         <div class="w-full lg:w-1/3 lg:px-4">
 					<?php get_template_part('template-parts/sidebar/sidebar-posts'); ?>
