2f7d890ad1adea7ea8978fabed7681393a8d3e14
Puker Who <nawwesele@gmail.com>
2022-07-12 19:26:04 +0300

    edit

--- single.php
+++ single.php
@@ -122,6 +122,18 @@
             <div class="content mb-10">
               <?php the_content(); ?>
             </div>
+            <div class="mb-10">
+              <!-- Single - bottom -->
+              <ins class="adsbygoogle"
+              style="display:block"
+              data-ad-client="ca-pub-6649504422654100"
+              data-ad-slot="2557183214"
+              data-ad-format="auto"
+              data-full-width-responsive="true"></ins>
+              <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+              </script>
+            </div>
           </article>
 
           <!-- Оценка -->
