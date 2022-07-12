e2cf56c3bcb28bb3f267bd40199eae141b4689f3
Puker Who <nawwesele@gmail.com>
2022-07-12 18:23:26 +0300

    edit

--- single.php
+++ single.php
@@ -107,6 +107,18 @@
             </div>
             <hr>
             <!-- END Автор -->
+            <div class="mb-10">
+              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6649504422654100" crossorigin="anonymous"></script>
+              <ins class="adsbygoogle"
+              style="display:block; text-align:center;"
+              data-ad-layout="in-article"
+              data-ad-format="fluid"
+              data-ad-client="ca-pub-6649504422654100"
+              data-ad-slot="2469838718"></ins>
+              <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+              </script>
+            </div>
             <div class="content mb-10">
               <?php the_content(); ?>
             </div>
