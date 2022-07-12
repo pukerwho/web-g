48747ccf672d01aaac2678804f655eb6ade65e26
Puker Who <nawwesele@gmail.com>
2022-07-12 16:35:24 +0300

    edit

--- page.php
+++ page.php
@@ -2,14 +2,14 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-  <main id="primary" class="page-padding">
+  <main id="primary" class="page-padding bg-white dark:bg-dark-lg">
     <div class="container">
       <div class="mb-12">
-        <div class="text-3xl lg:text-4xl text-center font-bold">
+        <div class="text-3xl lg:text-4xl text-gray-800 dark:text-gray-200 text-center font-bold">
           <?php the_title(); ?>  
         </div>
       </div>
-      <div class="flex flex-col lg:flex-row lg:-mx-8 mb-12">
+      <div class="flex flex-col lg:flex-row lg:-mx-8 pb-12">
         <!-- Основной поток -->
         <div class="w-full lg:w-8/12 justify-center px-0 lg:px-8 mx-auto">
           <div class="content">
