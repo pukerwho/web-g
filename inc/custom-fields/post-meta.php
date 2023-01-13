f05268dc61f816c02fd5af564e6355e64eb1ce1f
Puker Who <nawwesele@gmail.com>
2023-01-13 21:48:12 +0200

    edit

--- inc/custom-fields/post-meta.php
+++ inc/custom-fields/post-meta.php
@@ -8,8 +8,10 @@ function crb_post_theme_options() {
 	Container::make( 'post_meta', 'More' )
     ->where( 'post_type', '=', 'post' )
     ->add_fields( array(
+      Field::make( 'html', 'crb_heading_seo', __( 'SEO Heading' ) )->set_html( sprintf( '<b>SEO</b>' ) ),
       Field::make( 'text', 'crb_post_title', 'Title' ),
-      Field::make( 'textarea', 'crb_post_description', 'Description' ),
+      Field::make( 'textarea', 'crb_post_description', 'Description' )->set_attribute( 'maxLength', '144' ),
+      Field::make( 'text', 'crb_post_keywords', 'Keywords' ),
       Field::make( 'html', 'crb_heading_author', __( 'INFO Heading' ) )->set_html( sprintf( '<b>АВТОР</b>' ) ),
       Field::make( 'text', 'crb_post_author', 'Автор' ),
       Field::make( 'text', 'crb_post_author_instagram', 'Інстаграм автора' ),
