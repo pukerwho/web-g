b1d30a61deae1e9db81764b302cb62d25f5f38ce
Puker Who <nawwesele@gmail.com>
2023-02-07 19:44:06 +0200

    edit

--- inc/custom-fields/post-meta.php
+++ inc/custom-fields/post-meta.php
@@ -16,6 +16,7 @@ function crb_post_theme_options() {
       Field::make( 'text', 'crb_post_author', 'Автор' ),
       Field::make( 'text', 'crb_post_author_instagram', 'Інстаграм автора' ),
       Field::make( 'text', 'crb_post_author_facebook', 'Фейсбук автора' ),
+      Field::make( 'checkbox', 'crb_post_google_ads', 'Не показувати Google Ads' ),
       Field::make( 'association', 'crb_similar_links', 'Похожие ссылки')
       ->set_types( array(
         array(
