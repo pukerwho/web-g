a380ba2255289d4258e2596f09fb7715e6f95d53
Puker Who <nawwesele@gmail.com>
2024-01-31 00:08:28 +0200

    Edit

--- header.php
+++ header.php
@@ -94,11 +94,23 @@ if ( is_singular() ) {
 	      'menu_class' => 'menu flex flex-col'
 	    ]); ?> 
 		</div>
-    <div class="bg-dark-md p-4">
+    <div class="bg-dark-md flex justify-between p-4">
       <div class="flex items-center text-gray-200 -mx-2">
         <?php if (function_exists('pll_the_languages')) {
           pll_the_languages(); 
         } ?>
+      </div>
+      <div class="cursor-pointer">
+        <div class="hidden dark:block text-gray-200 js-toggle-light" data-light="off">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>
+        </div>
+        <div class="block dark:hidden text-gray-200 js-toggle-light" data-light="on">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+          </svg>
+        </div>
       </div>
     </div>
 	</div>
